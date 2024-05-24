780be91c29cd1cd06f060d1491c142546d4d234f
nikoloziGogishvili <giorgigogishvili971@gmail.com>
2024-05-24 17:29:53 +0400

    sixth commit

---
+++ level 05/classwork.py
@@ -0,0 +1,10 @@
+
+
+name = input("enter your name: ")
+print("your name:" + " " +  name)
+
+age = input("enter your age:")
+print("your age:" + " " + age)
+
+height = input("enter your height:")
+print("your height:" + " " +  height)
