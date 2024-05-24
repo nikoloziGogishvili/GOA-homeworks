e9648388bf2f458d73199bd4d41c69c9cc7bd59a
nikoloziGogishvili <giorgigogishvili971@gmail.com>
2024-05-24 17:29:10 +0400

    fifth commit

---
+++ level 04/homework.py
@@ -0,0 +1,657 @@
+from turtle import *
+
+shape("turtle")
+
+speed(100000000000)
+width(5)
+
+
+penup()              # ვიწყებთ სასახლის აშენებას
+goto(-550, -400)
+pendown()
+
+
+
+forward(1200)
+goto(-550, -400)
+
+
+left(90)
+forward(500)
+right(90)
+forward(200)
+right(90)
+forward(500)
+left(180)
+forward(500)
+right(90)
+forward(40)
+left(90)
+forward(80)
+left(90)
+forward(40)
+left(90)
+forward(40)
+right(90)
+forward(50)
+right(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+right(90)
+forward(50)
+right(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(80)
+left(90)
+forward(60)
+right(90)
+forward(500)
+left(90)
+forward(1400)
+left(90)
+
+
+
+forward(500)
+left(90)
+forward(200)
+left(90)
+forward(500)
+right(180)
+forward(500)
+left(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(40)
+left(90)
+forward(50)
+left(90)
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(40)
+left(90)
+forward(50)
+left(90)
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(60)
+left(90)
+forward(500)
+right(90)
+forward(200)
+right(90)
+forward(400)
+left(90)
+
+
+
+forward(1000)
+right(180)
+forward(200)
+left(90)
+forward(300)
+left(90)
+forward(30)
+right(180)
+forward(600)
+forward(30)
+right(180)
+forward(30)
+left(90)
+forward(300)
+left(180)
+forward(300)
+right(90)
+forward(30)
+left(180)
+right(30)
+forward(350)
+left(58)
+forward(375)
+left(62)
+left(90)
+forward(34)
+right(90)
+forward(299)
+right(90)
+forward(200)
+left(90)
+forward(400)
+left(90)                   # აქ დასრულდა სასახლის კედლები
+
+
+
+forward(350)                # აქედან ვაკეთებთ კარებს
+left(90)
+
+forward(230)
+right(45)
+forward(100)
+right(90)
+forward(100)
+right(45)
+forward(230)
+                          #აქედან ვიწყებთ აგურებით გაფორმებას
+penup()
+goto(-365, -380)
+pendown()
+
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+
+penup()
+goto(-455, -290)
+pendown()
+
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+
+penup()
+goto(-365, -200)
+pendown()
+
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+
+penup()
+goto(-455, -110)
+pendown()
+
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+
+penup()
+goto(-365, -20)
+pendown()
+
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+
+penup()
+goto(830, -380)
+pendown()
+
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+
+penup()
+goto(740, -290)
+pendown()
+
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+
+penup()
+goto(830, -200)
+pendown()
+
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+
+penup()
+goto(740, -110)
+pendown()
+
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+
+penup()
+goto(830, -20)
+pendown()
+
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+
+penup()
+goto(740, 40)
+pendown()
+
+right(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+                               # აქ დავასრულეთ კედლების აგურებით გაფორმება
+penup()
+goto(-130, -170)
+pendown()
+
+right(90)
+forward(100)
+right(90)
+forward(80)
+right(45)
+forward(70)
+right(90)
+forward(70)
+right(45)
+forward(80)
+
+penup()
+goto(300, -170)
+pendown()
+
+left(90)
+forward(150)
+left(90)
+forward(90)
+left(60)
+forward(90)
+left(60)
+forward(90)
+left(60)
+forward(90)
+left(90)
+forward(30)
+
+penup()
+goto(-80, 60)
+pendown()
+
+forward(100)
+left(90)
+forward(80)
+left(45)
+forward(70)
+left(90)
+forward(70)
+left(45)
+forward(80)
+
+penup()
+goto(200, 60)
+pendown()
+
+left(90)
+forward(150)
+left(90)
+forward(90)
+left(60)
+forward(90)
+left(60)                                              
+forward(90)
+left(60)
+forward(90)
+left(90)
+forward(30)                 # აქ ვასრულებთ ფანჯრების გაკეთებას
+                                      
+penup()                     # ვხატავთ მეფეს
+goto(-460, 230)
+pendown()
+
+right(45)
+forward(63)
+
+penup()
+goto(-460, 230)
+pendown()
+
+right(90)
+forward(63)
+
+penup()
+goto(-460, 230)
+pendown()
+
+right(135)
+forward(120)
+
+penup()
+goto(-460, 280)
+pendown()
+
+right(60)
+forward(70)
+
+penup()
+goto(-460, 280)
+pendown()
+
+left(120)
+forward(70)
+
+penup()
+goto(-460, 280)
+pendown()
+
+left(300)
+forward(70)
+
+penup()
+goto(-420, 393)
+pendown()
+
+circle (40)
+
+penup()
+goto(-442, 393)
+pendown()
+
+forward(5)
+
+penup()
+goto(-470, 393)
+pendown()
+
+forward(5)
+
+penup()
+goto(-450, 380)
+pendown()
+
+left(90)
+forward(9)
+
+penup()
+goto(-410, 433)
+pendown()
+
+color("yellow")       # ვიწყებთ მეფის გვირგვინის დახატვას
+forward(90)
+
+penup()
+goto(-420, 433)
+pendown()
+
+right(135)
+left(20)
+forward(45)
+left(140)
+forward(30)
+right(90)
+left(10)
+forward(51)
+left(110)
+forward(50)
+right(70)
+forward(30)
+left(130)
+forward(44)               # ვასრულებთ მეფის გვირგვინს
+
+penup()                   # ვხატავთ დროშას
+goto(-400, 230)
+pendown()
+
+color("black")
+right(25)
+left(180)
+forward(170)
+right(90)
+forward(180)
+right(90)
+forward(100)
+right(90)
+forward(180)
+
+penup()
+goto(-350, 380)
+pendown()                # დავასრულეთ დროშის დახატვა
+
+left(180)                # დროშას ვაწერთ GOA-ს
+circle(-20, -190)
+left(267)
+forward(17)
+left(90)
+forward(7)
+left(180)
+forward(9)
+
+penup()
+goto(-310, 340)
+pendown()
+
+circle(20)
+
+penup()
+goto(-280, 340)
+pendown()
+
+left(65)
+forward(40)
+right(150)
+forward(40)
+left(180)
+forward(15)
+left(45)
+forward(10)          # დავაწერეთ დროშას GOA
+
+penup()              # ვხატავთ დედოფალს
+goto(770, 245)
+pendown()
+right(55)
+right(180)
+left(20)
+forward(67)
+
+penup()
+goto(770, 245)
+pendown()
+
+right(50)
+forward(69)
+
+penup()
+goto(770, 245)
+pendown()
+
+color("pink")
+begin_fill()
+left(28)
+left(90)
+forward(70)
+left(120)
+forward(137)
+
+penup()
+goto(770, 245)
+pendown()
+
+right(30)
+left(90)
+forward(70)
+right(120)
+forward(137)
+end_fill()
+
+penup()
+goto(770, 245)
+pendown()
+
+left(30)
+forward(120)
+
+penup()
+goto(815, 405)
+pendown()
+
+color("black")
+circle (40)
+
+penup()
+goto(795, 405)
+pendown()
+
+forward(6)
+
+penup()
+goto(760, 405)
+pendown()
+
+forward(6)
+
+penup()
+goto(787, 384)
+pendown()
+
+left(90)
+forward(15)
+
+penup()
+goto(790, 330)
+pendown()
+
+left(180)
+left(45)
+forward(70)
+
+penup()
+goto(750, 330)
+pendown()
+
+right(45)
+left(180)
+right(45)
+forward(70)
+
+penup()
+goto(800, 445)
+pendown()
+
+left(45)
+color("red")
+forward(60)
+left(90)
+forward(60)
+left(180)
+forward(60)
+left(135)
+forward(60)
+right(45)
+left(90)
+left(135)
+forward(60)
+right(165)
+forward(60)
+
+penup()
+goto(800, 445)
+pendown()
+
+left(45)
+forward(60)
+right(180)
+forward(60)
+right(180)
+left(20)
+forward(60)
+right(20)
+right(180)
+forward(60)
+right(160)
+forward(60)         # დავხატეთ დედოფალი
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exitonclick()
