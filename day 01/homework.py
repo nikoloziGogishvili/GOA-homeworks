cbe1dc6ca2fb7e68ded7711f4c60febc263258dc
nikoloziGogishvili <giorgigogishvili971@gmail.com>
2024-05-24 17:10:13 +0400

    second commit

---
+++ day 01/homework.py
@@ -0,0 +1,21 @@
+#ვადეკლარირებ str (string) ტიპის ცვლადს სახელად name და ვანიჭებ მნიშვნელობას (ნიკა)
+name = "nika"
+
+surname = "gogishvili"
+
+age = 16
+
+height = 180.5 
+
+
+print("me var " + name + " " + surname + " " +  str(age) + " wlis " + "chemi simaglea " + str(height) + " ukve momwons GOA")
+
+A = age
+
+B = height
+
+C = A + B
+
+print(C)
+
+
