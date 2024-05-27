2e14c93cde8cd480bf33145e4c0c4c4d62d05048
nikoloziGogishvili <giorgigogishvili971@gmail.com>
2024-05-27 14:39:25 +0400

    seventh commit

---
+++ level 06/homework.py
@@ -0,0 +1,115 @@
+# 2) გამოიყენეთ ყველა კონვერტაციის ფუნქცია და გააკეთეთ თითოეულზე 5-5 მაგალითი,
+# დააკვირდით და გაანალიზეთ როგორ მუშაობს თითოეული ფუნქცია(რა დროს მოგცათ ერორი და რა დროს არა)
+
+number = 9
+print(float(number))
+print(int(number))
+print(str(number))
+
+number = "9"
+print(float(number))
+print(str(number))
+print(int(number))
+
+number = 9.5
+print(float(number))
+print(str(number))
+print(int(number))
+
+
+number = 5
+print(float(number))
+print(str(number))
+print(int(number))
+
+number = "5"
+print(float(number))
+print(str(number))
+print(int(number))
+
+number = 5.3
+print(float(number))
+print(str(number))
+print(int(number))
+
+
+number = 35
+print(float(number))
+print(str(number))
+print(int(number))
+
+number = "35"
+print(float(number))
+print(str(number))
+print(int(number))
+
+number = 35.1
+print(float(number))
+print(str(number))
+print(int(number))
+
+
+number = 90
+print(float(number))
+print(str(number))
+print(int(number))
+
+number = "90"
+print(float(number))
+print(str(number))
+print(int(number))
+
+number = 90.9
+print(float(number))
+print(str(number))
+print(int(number))
+
+
+number = 2
+print(float(number))
+print(str(number))
+print(int(number))
+
+number = "2"
+print(float(number))
+print(str(number))
+print(int(number))
+
+number = 2.2
+print(float(number))
+print(str(number))
+print(int(number))
+
+
+
+# 3) შექმენით პროგრამა სადაც მომხმარებელი შემოიტანს 5 რიცხვს,
+# ხოლო ამ 5 რიცხვს შორის გამოიყენეთ ყველა არითმეტიკული ოპერაცია (// და %_იც),
+# საბოლოოდ დაბეჭდეთ შედეგები ტერმინალში + ახსენით თითოეული ნაწილი კოდის რატომ
+# დაწერეთ კონკრეტული ხაზი და რას აკეთებს.
+
+
+# მომხმარებელს შემოვატანინე 5 ინტეჯერი და შევინახე ცვლადებში 
+num1 = int(input("enter number: "))        
+num2 = int(input("enter number: "))
+num3 = int(input("enter number: "))
+num4 = int(input("enter number: "))
+num5 = int(input("enter number: "))
+
+# შემოტანილი რიცხვებით გავაკეთე არითმეტიკული ოპერაციები და დავბეჭდე ტერმინალში
+print(num1 + num2)
+print(num4 - num3)
+print(num4 / num1)
+print(num5 // num3)
+print(num2 % num5)
+print(num1 * num3)
+
+
+
+
+
+
+
+
+
+
+
