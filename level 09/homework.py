c0107b6f298c44d760a0495fd6de9c842618e21e
nikoloziGogishvili <giorgigogishvili971@gmail.com>
2024-06-03 15:58:21 +0400

    tenth commit

---
+++ level 09/homework.py
@@ -0,0 +1,84 @@
+# For loop 
+# 3)0-იდან 20-ის ჩათვლით დაპრინტეთ ყველა მთელი რიცხვი
+
+# for i in range(20 + 1):
+#     print(i)
+
+
+
+
+# 4)დაპრინტეთ პირველი 10 ნატურალური რიცხვი 
+
+# for i in range(1, 10 + 1):
+#     print(i)
+
+
+# 5)დაპტინტეთ 0-იდან 100-ის ჩათვლით ლუწი რიცხვები
+
+# for i in range(0, 101, 2):
+#     print(i)
+
+# 6)შემოატანინეთ მომხმარებელს რიცხვი და დაადგინეთ არის თუ არა დადებითი უარყოფითი ან ნულის ტოლი 
+
+# num1 = int(input("enter number: "))
+# if num1 == 0:
+#     print("this number is zero")
+
+# elif num1 > 0:
+#     print("this number is positive number")
+
+# else:
+#     print("this number is negative")
+
+
+
+# 7)შემოატანინეთ მომხმარებელს მისი ასაკი თუ მისი ასაკი 18 წელზე მეტია დაუპრინტეთ “you are adult” თუ 18 წელზე ნაკლები “you are virgin”
+
+# age = int(input("enter your age: "))
+# if age > 18:
+#     print("you are adult")
+
+# else:
+#     print("you are virgin")
+
+
+
+
+# 8)დაწერეთ ისეთი პროგრამა რომელიც მომხმარებელს უპრინტავს კვირის დღეს მომხმარებლის შემოტანილი რიცხვის მიხედვით (1 არის ორშაბათი, 2 სამშაბათი და ა.შ) გამოიყენეთ if elif else
+
+num = int(input("enter number: "))
+
+if num == 1:
+    print("Monday")
+
+elif num == 2:
+    print("Tuesday")
+
+elif num == 3:
+    print("Wednesday")
+
+elif num == 4:
+    print("Thursday")
+
+elif num == 5:
+    print("Friday")
+
+elif num == 6:
+    print("saturday")
+
+elif num == 7:
+    print("Sunday")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
