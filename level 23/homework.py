caec64fbe22d0756317abffadeff846112e76511
nikoloziGogishvili <giorgigogishvili971@gmail.com>
2024-07-26 14:22:01 +0400

    twenty-fourth commit

---
+++ level 23/homework.py
@@ -0,0 +1,38 @@
+# 1. შექმენით ფუნქცია, რომელიც იღებს რაიმე რიცხვს და აბრუნებს მასზე 5'ით მეტს.
+
+# def number_taker():
+#     num = int(input("please enter any number: "))
+#     result = num + 5
+#     print(result)
+
+# number_taker()
+
+
+# 2. შექმენით ფუნქცია, რომელიც იღებს ორ integer'ს და აბრუნებს მათ ნამრავლს
+
+# def integer_taker(a, b):
+#     print(a * b)
+
+# integer_taker(3, 4)
+
+
+# 3. შექმენით ფუნქცია, რომელიც იღებს string'ს ამ string'ის სიგრძეს (გამოიყენეთ ფუნქცია len()).
+
+# def string_taker(a):
+#     print(len(a))
+
+# string_taker("GOA")
+
+
+# 5. შექმენით ფუნქცია, რომელიც იღებს string'ს და აბრუნებს True-ს თუ ის არის Palindrome (ანუ იგივენაირად იკითხება მარცნიდანაც და მარჯვნიდანაც მაგალითად: "wow") და სხვა შემთხვევაში False-ს.
+
+# def isPalindrome(s):
+#     return s == s[::-1]
+
+# s = "malayalam"
+# ans = isPalindrome(s)
+
+# if ans:
+#     print("Yes")
+# else:
+#     print("No")
