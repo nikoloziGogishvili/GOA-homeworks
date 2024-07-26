c25b2e929c1ee647c3b2f07fd221ac9b9239d78f
nikoloziGogishvili <giorgigogishvili971@gmail.com>
2024-07-26 14:13:50 +0400

    seventeenth commit

---
+++ level 16/homework.py
@@ -0,0 +1,58 @@
+# 1) შექმენით სია, სადაც შეიტანთ თქვენი ოჯახის წევრების სახელებს. გამოიყენეთ indexing და დაბეჭდეთ ყველას სახელი ცალ-ცალკე.
+
+# family = ["Maka", "Maia", "Giorgi", "Gio", "Nika"]
+# print(family)
+# print(family[0])
+# print(family[1])
+# print(family[2])
+# print(family[3])
+# print(family[4])
+
+# 2) ექმენით სია, სადაც გექნებათ 1-იდან 10-ის ჩათვლით რიცხვები. ჯერ გამოიტანეთ სიის პირველი, ხოლო შემდეგ ბოლო ელემენტი.
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(numbers[0])
+# print(len(numbers))
+
+# 3) შექმენით სია, სადაც გექნებათ 10-იდან 20-ის ჩათვლით რიცხვები. გაიხსენეთ ის ფაქტი, რომ თქვენ შეგიძლიათ უკვე შექმნილი სიის ელემენტების შეცვლა
+# შეცვალეთ სიის ლუწ ინდექსებზე მყოფი ელემენტები და მათ მაგივრად დაწერეთ 1.
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# nums[1:3] = [1]
+# print(nums)
+
+# 4) მომხმარებელს შეეკითხეთ სახელი, გვარი, ასაკი, საცხოვრებელი ადგილი და მეილი. ყველა შეინახეთ ცვლადებში და შემდეგ ჩაამატეთ სიაში, საბოლოოდ კი დაბეჭდეთ ეს სია.
+
+# name = input("please enter your name: ")
+# surname = input("please enter your surname: ")
+# age = int(input("please enter your age: "))
+# Dwelling_place = input("please enter your Dwelling place: ")
+# mail = input("please enter your mail: ")
+
+# person_info = [name, surname, age, Dwelling_place, mail]
+# print(person_info)
+
+# 5) ცვლადში შეინახეთ თქვენი გვარი. გამოიყენეთ indexing და მისი თითოეული ასო დაბეჭდეთ.
+
+# surname = "gogishvili"
+# print(len(surname))
+# print(surname[0])
+# print(surname[1])
+# print(surname[2])
+# print(surname[3])
+# print(surname[4])
+# print(surname[5])
+# print(surname[6])
+# print(surname[7])
+# print(surname[8])
+# print(surname[9])
+
+
+
+
+
+
+
+
+
+
