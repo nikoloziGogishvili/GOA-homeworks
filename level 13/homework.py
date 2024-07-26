eb8e46f3ab9af54f384f37b22710475c3d962bc8
nikoloziGogishvili <giorgigogishvili971@gmail.com>
2024-07-26 14:10:31 +0400

    fourteenth commit

---
+++ level 13/homework.py
@@ -0,0 +1,87 @@
+# გაიარეთ while ციკლები და გააკეთეტ 5 მაგალითი while ციკლზე (თქვენ მოიფიქრეთ)
+
+
+# num = 0
+# while num != 5:
+#     print("GOA")
+#     num = num + 1
+
+# num = 0
+# while num <= 9:
+#     print("nika")
+#     num = num + 1
+
+# num = 0
+# while num != 7:
+#     print(16)
+#     num = num + 1
+
+# word = input("please enter any word: ")
+# num = 0
+# while num != 10:
+#     print(word)
+#     num = num + 1
+
+# num1 = input("please enter any number: ")
+# num = 0
+# while num != 2:
+#     print(num1)
+#     num = num + 1
+
+
+
+# გაიარეთ for ციკლი და გააკეთეთ 5 მაგალითი for ციკლებზე
+
+# for i in range(3, 10):
+#     print(i)
+
+# for i in range(1, 8):
+#     print(i)
+
+# for i in range(90, 100):
+#     print(i)
+
+# for i in range(1, 9):
+#     print(i)
+
+# for i in range(67, 76):
+#     print(i)
+
+# for i in range(70, 77):
+#     print(i)
+
+
+
+# გააკეთეთ 5 მაგალითი if-else
+
+# num = int(input("please any number: "))
+# if num == 9:
+#     print("you won")
+# else:
+#     print("you lost")
+
+# if 9 != 9:
+#     print("TRUE")
+# else:
+#     print("FALSE")
+
+# password = input("please enter your password: ")
+# password1 = "nika1233"
+# if password == password1:
+#     print("corret password")
+# else:
+#     print("wrong password")
+
+# num = int(input("please enter any number: "))
+# if num == 7:
+#     print("correct number")
+# elif num == 3:
+#    print("correct number")
+# else:
+#     print("wrong number")
+
+# num = int(input("please enter number: "))
+# if num != 12:
+#     print("try again!")
+# else:
+#     print("Good job!")
