5de0c9bd3aeb322c97c015f4c16b453b387b8bc6
nikoloziGogishvili <giorgigogishvili971@gmail.com>
2024-07-26 14:16:08 +0400

    Nineteenth commit

---
+++ level 18/homework.py
@@ -0,0 +1,17 @@
+
+# fruits = ["apple", "banana", "cherry"]
+# for i in fruits:
+#   print(i)
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for i in fruits:
+#   if i == "banana":
+#     continue
+#   print(i)
+
+# fruits = ["apple", "banana", "cherry"]
+# for i in fruits:
+#   if i == "banana":
+#     break
+#   print(i)
