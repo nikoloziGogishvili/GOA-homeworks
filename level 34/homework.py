6481caa4c86de158a946835089378f515540720c
nikoloziGogishvili <giorgigogishvili971@gmail.com>
2024-08-14 18:37:43 +0400

    thirty-fifth

---
+++ level 34/homework.py
@@ -0,0 +1,33 @@
+# 3) იმუშავეთ split-ის გამოყენებაზე: შექმენით 10 string და თითოეულზე გამოიყენეთ split ბრძანება, მოახდინეთ გახლეჩვა ნებისმიერი სიმბოლოთი
+
+# name = "G1O1A"
+# print(name.split("1"))
+
+# name1 = "nika gogishvili"
+# print(name1.split(" "))
+
+# name2 = "n/i/k/a gogishvili"
+# print(name1.split("/"))
+
+# name3 = "namcxvari"
+# print(name3.split("x"))
+
+# name4 = "castle"
+# print(name4.split("c"))
+
+# abc = "a b c"
+# print(abc.split("b"))
+
+# question = "how are you?"
+# print(question.split("?"))
+
+# numbers = "1 2 3 4 5 6 7 8 9"
+# print(numbers.split("5"))
+
+# fruit = "apple"
+# print(fruit.split("p"))
+
+# fruit = "apple"
+# print(fruit.split("q"))
+
+
